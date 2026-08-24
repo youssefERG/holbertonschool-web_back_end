b1c8847d0cb4bb6f7ebb6ce2328143bbc4ee0943
youssefERG <erengamer555@gmail.com>
2026-08-24 14:42:23 +0200

    task 1,2  correction + task 3 and 4

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-"""This module provides a helper function for basic pagination."""
+""" function returns a tuple containing a start index
+    and an end index corresponding to the range of indexes
+    to return in a list for those particular pagination parameters."""
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    """Return the start and end indexes for a requested pagination page."""
+def index_range(page, page_size):
+    """ function returns a tuple containing a start index
+    and an end index corresponding to the range of indexes
+    to return in a list for those particular pagination parameters."""
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    return (start_index, end_index)
+    return start_index, end_index
--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 """This module returns the appropriate page of the dataset"""
 import csv
@@ -40,3 +41,17 @@ class Server:
 
         start_index, end_index = index_range(page, page_size)
         return self.dataset()[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict[dict]:
+        """Return a page of data with hypermedia pagination metadata."""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages,
+        }
