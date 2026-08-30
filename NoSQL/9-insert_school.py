35819653c1e053972f60f2c5b67717b5fd8e3815
youssefERG <erengamer555@gmail.com>
2026-08-31 00:15:00 +0200

    finaly some progress

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""This module introduces python NoSQL manipulation"""
+"""Insert a document into a MongoDB collection."""
 
 
 def insert_school(mongo_collection, **kwargs):
-    """function that inserts a new document in a collection based on kwargs"""
+    """Insert kwargs into mongo_collection and return its new ID."""
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
