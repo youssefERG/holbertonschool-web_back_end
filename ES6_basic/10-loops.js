c0b2799c8d639fd0a4cf5d391d9da26f73f61305
youssefERG <erengamer555@gmail.com>
2026-09-03 22:49:52 +0200

    tired

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+	for (const value of array) {
+	  const index = array.indexOf(value);
+	  array[index] = appendString + value;
+	}
+
+	return array;
+  }
