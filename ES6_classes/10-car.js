c4eb71b61acb3f9681d41a97be53edc8597130c7
youssefERG <erengamer555@gmail.com>
2026-09-04 06:41:30 +0200

    es6 classes

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+export default class Car {
+	constructor(brand, motor, color) {
+	  this._brand = brand;
+	  this._motor = motor;
+	  this._color = color;
+	}
+
+	cloneCar() {
+	  return new this.constructor[Symbol.species]();
+	}
+
+	static get [Symbol.species]() {
+	  return this;
+	}
+  }
---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    }
+
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
